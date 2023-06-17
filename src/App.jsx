90568890c063453e9d324c42255bcefc5bdd3209
Felipe Losano <felipelosano@outlook.com>
2023-06-16 23:35:33 -0300

    correção

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import './App.css';
 
 import NavBar from './components/NavBar/NavBar';
-import Card from './components/Card/Card';
 
 function App() {
 	return (
 		<>
 			<NavBar />
-			<Card>
 				<h1>teste</h1>
-			</Card>
 		</>
 	);
 }
