b2eaa6b686d4af512d6363e2720c40c59360ef20
Felipe Losano <felipelosano@Outlook.com>
2023-06-19 16:50:00 -0300

    Inicio da home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
+import { FaReact, FaNodeJs, FaJava } from 'react-icons/fa';
 import './App.css';
 
 import NavBar from './components/NavBar/NavBar';
-import CardHome from './components/CardHome/CardHome';
 
 function App() {
+	const tecSizes = 60;
+
 	return (
 		<div className='container'>
-			<NavBar />
+			<header>
+				<NavBar />
+			</header>
+
+			<main>
+				<section id='Home'>
+					<div className='card'>
+						<div className='home'>
+							<div className='div-img'>
+								<img src='./img/Felipe.jpg' alt='' className='home-img' />
+							</div>
+							<div className='div-title'>
+								<h1>Felipe Losano</h1>
+								<p>Desenvolvedor Full-Stack</p>
+							</div>
+						</div>
+					</div>
+					<div className='card'>
+						<div className='technologies'>
+							<h1 className='tec-title'>Tecnologias</h1>
+							<div className='tec-logos'>
+								<ul>
+									<li>
+										<FaReact color='#61dbfb' size={tecSizes} />
+									</li>
+									<li>
+										<FaNodeJs color='#3c873a' size={tecSizes} />
+									</li>
+									<li>
+										<FaJava color='#f89820' size={tecSizes} />
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</section>
+			</main>
+
+			<footer></footer>
 		</div>
 	);
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,26 @@
 import styles from './NavBar.module.css';
+import { FaReact } from 'react-icons/fa';
 
 const NavBar = () => {
 	return (
 		<div className={styles.navbar}>
-			<img className={styles.logo} src='img\react-logo.png' alt='react-logo' />
+			{/* <img className={styles.logo} src='img\react-logo.png' alt='react-logo' /> */}
+			<FaReact className={styles.logo} />
 			<nav>
-				<a href='#Home'>Home</a>
-				<a href='#About'>About</a>
-				<a href='#Projects'>Projects</a>
-				<a href='#Contact'>Contact</a>
+				<ul>
+					<li>
+						<a href='#Home'>Home</a>
+					</li>
+					<li>
+						<a href='#About'>About</a>
+					</li>
+					<li>
+						<a href='#Projects'>Projects</a>
+					</li>
+					<li>
+						<a href='#Contact'>Contact</a>
+					</li>
+				</ul>
 			</nav>
 		</div>
 	);
